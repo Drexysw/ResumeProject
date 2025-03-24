17dea31335eff485780edebe615ed08a9caaccd1
Danislav <knevdani@gmail.com>
2025-03-24 12:16:03 +0200

    created a function and added an event listhener to the button for dowloading cv. made animations in the main page and fixed the links about the other pages

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 function toggleFullScreen() {
     if (!document.fullscreenElement) {
         document.documentElement.requestFullscreen();
@@ -5,3 +6,20 @@ function toggleFullScreen() {
         document.exitFullscreen();
     }
 } 
+function downloadCV() {
+    // Adjust the file path and filename as needed
+    const filePath = '/assets/files/D.Kanev CV.pdf';
+
+    // Create a temporary anchor element
+    const link = document.createElement('a');
+    link.href = filePath;
+    link.download = 'D.Kanev CV.pdf';
+
+    // Append the anchor to the document, trigger click, and remove it
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+function redirectToContact() {
+    window.location.href = '/pages/contact.html';
+}
